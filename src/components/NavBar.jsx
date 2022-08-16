372f73fd0618ea986d049e5aa2ef293ded12a70f
nasanchez7 <nadirblanco02@gmail.com>
2022-08-16 16:30:17 -0300

    incorporando navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+const Nav = () => {
+    return(
+        <nav>
+            <ul>
+                <a href=''><li>INICIO</li></a>
+                <a href=''><li>MENU</li></a>
+            </ul>
+            <h2>iBurger</h2>
+            <ul>
+                <a href=''><li>CONTACTO</li></a>
+                <a href=''><li>LOCALES</li></a>
+            </ul>
+      </nav>
+    );
+}
+
+export default Nav;
