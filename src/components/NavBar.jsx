e8ebcd0d36d5faa83155f46cb27c4f514a8cd483
nasanchez7 <nadirblanco02@gmail.com>
2022-08-17 16:39:41 -0300

    agregando logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,10 @@ const Nav = () => {
                 <a href=''><li>Inicio</li></a>
                 <a href=''><li>Menu</li></a>
             </ul>
-            <h2>iBurger</h2>
+            <div className="logo">
+                <a href="./index.html"><img src="./burgersvg.png" alt="burgerlogo"/></a>  
+                <h2><a href="./index.html">iBurger</a></h2>
+            </div>
             <ul>
                 <a href=''><li>Contacto</li></a>
                 <a href=''><li>Locales</li></a>
